142d8917c57afab743dca367dc57ac9b155d8c3e
Dmytro Zakharov <zamdmytro@gmail.com>
2023-06-06 22:47:33 +0300

    :ambulance: remove migration structures since they do not work :)

--- kv.go
+++ kv.go
@@ -5,6 +5,8 @@ import (
 	"github.com/Masterminds/squirrel"
 	"github.com/fatih/structs"
 	"gitlab.com/distributed_lab/kit/pgdb"
+	"gitlab.com/distributed_lab/logan/v3"
+	"gitlab.com/distributed_lab/logan/v3/errors"
 )
 
 // KeyValue is an object stored in the key value storage
@@ -20,11 +22,15 @@ type KeyValueQ interface {
 	New() KeyValueQ
 	// Get is a function to get a value from the storage based on the key
 	Get(key string) (*KeyValue, error)
+	// MustGet is a function that tries retrieving a value but panics if it fails
+	MustGet(key string) *KeyValue
 	// Upsert updates value if there is one, insert if no
 	Upsert(KeyValue) error
 	// LockingGet reads row and locks the row for reading and updating
 	// until the end of the current transaction
 	LockingGet(key string) (*KeyValue, error)
+	// MustLockingGet does the same thing as LockingGet, but panics on error
+	MustLockingGet(key string) *KeyValue
 }
 
 const (
@@ -63,8 +69,24 @@ func (q *keyValueQ) Get(key string) (*KeyValue, error) {
 	return q.get(key, false)
 }
 
+func (q *keyValueQ) MustGet(key string) *KeyValue {
+	value, err := q.Get(key)
+	if err != nil {
+		panic(errors.Wrap(err, "failed to get value by key", logan.F{"key": key}))
+	}
+	return value
+}
+
 func (q *keyValueQ) LockingGet(key string) (*KeyValue, error) {
 	return q.get(key, true)
+}
+
+func (q *keyValueQ) MustLockingGet(key string) *KeyValue {
+	value, err := q.LockingGet(key)
+	if err != nil {
+		panic(errors.Wrap(err, "failed to locking get value by key", logan.F{"key": key}))
+	}
+	return value
 }
 
 func (q *keyValueQ) get(key string, forUpdate bool) (*KeyValue, error) {
